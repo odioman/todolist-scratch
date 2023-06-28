03a29880b63616c11e3bc997f0fa16303c3c6a49
Thomas Koshy <ttkoshy@gmail.com>
2023-06-28 19:45:44 -0400

    edit button functionality not working properly

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const input = document.querySelector('.input')
 const ul = document.querySelector('.todoList')
 
 
-//add a check mark when clicking on li
 
 
 //create a new li to append to todoList
@@ -25,10 +24,14 @@ function newElement() {
     span.innerHTML = '<i class="fa-solid fa-trash-can"></i>'
     span.className = 'close';
     li.appendChild(span);
+    const span2 = document.createElement("span");
+    span2.innerHTML = '<i class="fa-solid fa-pen"></i>';
+    span.className = 'edit';
+    li.appendChild(span2);
 
 
-    //ev.target.classList.toggle('checked')
-    checkbox.addEventListener('click', function(ev) {
+    //add a check mark when clicking on checkbox, styling changes with strikethrough and background color change;
+    checkbox.addEventListener('click', function() {
         if (this.checked) {
            li.style.textDecoration = 'line-through';
            li.style.background = '#888';
@@ -42,4 +45,30 @@ function newElement() {
         const div = this.parentElement;
         div.style.display = 'none';
     })
+
+    span2.addEventListener('click', editItem)
+}
+
+function editItem(event) {
+    let item = event.target.innerHTML;
+    let itemInput = document.createElement('input');
+    itemInput.type = 'text';
+    itemInput.value = item;
+    itemInput.classList.add('edit')
+    itemInput.addEventListener('keypress',saveItem);
+    itemInput.addEventListener('click', saveItem);
+    event.target.parentNode.prepend(itemInput);
+    event.target.remove();
+    itemInput.select();
+}
+
+function saveItem(event) {
+    let inputValue = event.target.value;
+    if (event.target.value.length > 0 && (event.keyCode === 13 || event.type === 'click')) {
+        let li = document.createElement('li');
+        li.addEventListener('click', editItem);
+        li.textContent = event.target.value;
+        event.target.parentNode.prepend(li);
+        event.target.remove()
+    }
 }
