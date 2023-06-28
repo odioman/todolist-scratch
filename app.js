3e030ae31b77e19f78617abbe43c57b68e5bd720
Thomas Koshy <ttkoshy@gmail.com>
2023-06-28 15:55:33 -0400

    styling for checked checkbox

--- app.js
+++ app.js
@@ -3,11 +3,7 @@ const ul = document.querySelector('.todoList')
 
 
 //add a check mark when clicking on li
-ul.addEventListener('click', function(ev) {
-    if (ev.target.tagName === 'li') {
-        ev.target.classList.toggle('checked');
-    }
-}, false);
+
 
 //create a new li to append to todoList
 function newElement() {
@@ -22,10 +18,25 @@ function newElement() {
         input.value= '';
     }
 
+    const checkbox = document.createElement('input');
+    checkbox.type = 'checkbox';
+    li.appendChild(checkbox);
     const span = document.createElement("span");
     span.innerHTML = '<i class="fa-solid fa-trash-can"></i>'
     span.className = 'close';
     li.appendChild(span);
+
+
+    //ev.target.classList.toggle('checked')
+    checkbox.addEventListener('click', function(ev) {
+        if (this.checked) {
+           li.style.textDecoration = 'line-through';
+           li.style.background = '#888';
+           li.style.color = '#fff';
+        } else {
+            li.style = 'auto';
+        }
+    });
 
     span.addEventListener('click', function(){
         const div = this.parentElement;
